b50d9354cd17bf9602ac8ed72226e1aa827bff05
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2022-12-19 15:18:40 +0000

    Add ship & gameboard objects with functions to allow ships to be placed

--- src/battleship.js
+++ src/battleship.js
@@ -1,8 +1,73 @@
 const battleShips = (() => {
+    //Factories
+    const ship = (coords, length, timesHit, sunk, hit, isSunk) => {
+        hit = () => {
+            timesHit++;
+        }
+
+        isSunk = () => {
+            if (timesHit >= length) {
+                sunk = true;
+            }
+        }
+
+        return { coords, length, timesHit, sunk, hit, isSunk }
+    };
+
+    const gameBoard = (grid) => {
+        const ships = [];
+
+        const generateGrid = () => {
+            const gridMap = [];
+
+            for(let i = 0; i <= 9; i++){
+                for(let j = 0; j <= 9; j++){
+                    gridMap.push(`[${i},${j}]`);
+                }
+            }
+
+            return gridMap;
+        }
+
+        const placeShip = (length, coords, direction) => {
+            const x = Number(coords[1]);
+            const y = Number(coords[3]);
+            const con = x + length >= 9 || y + length >= 9
+            if(con) return 'error';
+
+            const newCoords = [];
+            const newShip = ship('', length, 0, false);
+            if(direction === 'horizontal'){
+                for(let i = 0; i < length; i++){
+                    newCoords.push(`[${x+i},${y}]`);
+                }
+            }else if (direction === 'vertical'){
+                for(let i = 0; i < length; i++){
+                    newCoords.push(`[${x},${y+i}]`);
+                }
+            }
+
+            newShip.coords = newCoords;
+            ships.push(newShip);
+            return newShip
+        }
+
+        grid = generateGrid();
+
+        return { grid, placeShip, ships };
+    };
+
+    const player1Board = gameBoard();
+    player1Board.placeShip(4,'[0,0]','vertical')
+    player1Board.placeShip(4,'[0,0]','vertical')
 
     return {
-
+        ship,
+        player1Board,
     }
 })();
+
+
+console.log(battleShips.player1Board.ships);
 
 export default battleShips;
--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,5 +1,21 @@
 import battleShips from './battleship';
 
-test('test', () => {
-    expect(theFunction()).toMatch('hello');
+test('creates correct size grid', () => {
+    expect(battleShips.player1Board.grid.length).toBe(100);
+});
+
+test('coordinates are in correct spots', () => {
+    expect(battleShips.player1Board.grid[5]).toMatch('[0,5]');
+});
+
+test('ship gets placed horizontally', () => {
+    expect(battleShips.player1Board.placeShip(2,'[4,4]','horizontal').coords).toStrictEqual(["[4,4]","[5,4]"]);
+});
+
+test('ship gets placed vertically', () => {
+    expect(battleShips.player1Board.placeShip(4,'[0,0]','vertical').coords).toStrictEqual(['[0,0]','[0,1]','[0,2]','[0,3]']);
+});
+
+test('ship cannot be placed off board', () => {
+    expect(battleShips.player1Board.placeShip(5,'[9,0]','vertical')).toBe('error');
 });
