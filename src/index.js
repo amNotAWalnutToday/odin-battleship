702c43c4110f350b043a8083544dde3a4e2da38a
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2022-12-21 19:32:18 +0000

    Create ui & add title screen

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './styles.css';
 import battleShips from './battleship';
+import userInterface from './ui';
 
 const player1 = battleShips.player(1, true);
 const player2 = battleShips.player(2, true);
 const board = battleShips.gameBoard();
+const intro = userInterface.titleScreen();
 board.placeShip(5 ,'[0,0]', 'horizontal');
 
 const playGame = () => {
+    intro.loadTitleScreen();
     player1.takeTurn('[5,4]', board, player1, player2);
 };
 playGame();
---
+++ src/ui.js
@@ -0,0 +1,42 @@
+const userInterface = (() => {
+    const titleScreen = () => {
+        const container = document.querySelector('#container');
+        
+        const setContainer = () => {
+            container.setAttribute('class', 'title-screen');
+        }
+
+        const addTitle = () => {
+            const title = document.createElement('h1');
+            title.textContent = 'BattleShips';
+            container.appendChild(title);
+        }
+
+        const addButtons = () => { 
+            const pvpBtn = document.createElement('button');
+            const pvmBtn = document.createElement('button');
+            pvpBtn.textContent = 'Vs Player';
+            pvmBtn.textContent = 'Vs Computer';
+            container.appendChild(pvmBtn);
+            container.appendChild(pvpBtn); 
+        }
+
+        const addEvents = () => {
+            //add events to load specific games
+        };
+
+        const loadTitleScreen = () => { 
+            setContainer();
+            addTitle();
+            addButtons();
+        }
+
+        return { loadTitleScreen, };
+    };
+    
+    return {
+        titleScreen,
+    }
+})();
+
+export default userInterface;
